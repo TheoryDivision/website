e6b263b78808f32fc455b12856c93ff186319b76
Arda Durmaz <axd497@case.edu>
2025-02-24 22:15:47 -0500

    Regular updates

--- js/header.js
+++ js/header.js
@@ -1,8 +1,8 @@
-document.write('<h1><a href="https://my.clevelandclinic.org/"><img src="images/logo.png" alt="Theory Division @ Cleveland Clinic" id="logo"/></a>\
+document.write('<h1><a href="https://theorydi.vision/index.html/"><img src="images/logo.png" alt="Theory Division @ Cleveland Clinic" id="logo"/></a>\
 				| <a href="index.html">Theory Division</a></h1>\
 				<nav id="nav">\
 					<ul>\
-						<li><a href="https://my.clevelandclinic.org/" class="special"><img src="images/altlogo.png" alt="Theory Division @ Cleveland Clinic"  class="logospecial"/></a><a href="https://www.lerner.ccf.org/thor/scott/lab/" class="special">Theory Division</a></li>\
+						<li><a href="https://theorydi.vision/index.html/" class="special"><img src="images/altlogo.png" alt="Theory Division @ Cleveland Clinic"  class="logospecial"/></a><a href="https://www.lerner.ccf.org/thor/scott/lab/" class="special">Theory Division</a></li>\
 						<li><a href="people.html">People</a></li>\
 						<li><a href="research.html">Research</a></li>\
 						<li><a href="publications.html">Publications</a></li>\
